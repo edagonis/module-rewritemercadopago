def851ee36abc104950c8435b852d9dd0d20b85a
edusorcerer <eduardofidera@gmail.com>
2019-11-07 14:21:09 -0300

    Change system config fields and add fetching example on model

--- Model/Preference/Basic.php
+++ Model/Preference/Basic.php
@@ -5,8 +5,11 @@ namespace Avanti\RewriteMercadoPago\Model\Preference;
 use MercadoPago\Core\Helper\ConfigData;
 use Magento\Store\Model\ScopeInterface;
 
+const PATH_TITLE = 'rewritemercadopago/general/auth_token';
+
 class Basic extends \MercadoPago\Core\Model\Preference\Basic
 {
+
     /**
      * @return array
      * @throws Exception
@@ -14,6 +17,8 @@ class Basic extends \MercadoPago\Core\Model\Preference\Basic
     protected function getConfig()
     {
         $config = parent::getConfig();
+
+        var_dump($this->_scopeConfig->getValue(PATH_TITLE, ScopeInterface::SCOPE_STORE));die;
         
         return $config;
     }
