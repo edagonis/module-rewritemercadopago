2f26fdaf24465e4d5dd2d5beee1097bd77ce9e91
edusorcerer <eduardofidera@gmail.com>
2019-11-07 14:53:26 -0300

    Add sample regions data on model

--- Model/Preference/Basic.php
+++ Model/Preference/Basic.php
@@ -7,6 +7,43 @@ use Magento\Store\Model\ScopeInterface;
 
 const PATH_TITLE = 'rewritemercadopago/general/auth_token';
 
+const ALL_STATES = array(
+    "AC" => "Acre",
+    "AL" => "Alagoas",
+    "AM" => "Amazonas",
+    "AP" => "Amapá",
+    "BA" => "Bahia",
+    "CE" => "Ceará",
+    "DF" => "Distrito Federal",
+    "ES" => "Espírito Santo",
+    "GO" => "Goiás",
+    "MA" => "Maranhão",
+    "MG" => "Minas Gerais",
+    "MS" => "Mato Grosso do Sul",
+    "MT" => "Mato Grosso",
+    "PA" => "Pará",
+    "PB" => "Paraíba",
+    "PE" => "Pernambuco",
+    "PI" => "Piauí",
+    "PR" => "Paraná",
+    "RJ" => "Rio de Janeiro",
+    "RN" => "Rio Grande do Norte",
+    "RO" => "Rondônia",
+    "RR" => "Roraima",
+    "RS" => "Rio Grande do Sul",
+    "SC" => "Santa Catarina",
+    "SE" => "Sergipe",
+    "SP" => "São Paulo",
+    "TO" => "Tocantins",
+);
+
+const MAPPED_REGIONS = array(
+    0 => array(
+        'name' => 'Sul',
+        'states' => ['teste', 'wtf']
+    )
+);
+
 class Basic extends \MercadoPago\Core\Model\Preference\Basic
 {
 
@@ -18,6 +55,7 @@ class Basic extends \MercadoPago\Core\Model\Preference\Basic
     {
         $config = parent::getConfig();
 
+        var_dump(MAPPED_REGIONS);die;
         var_dump($this->_scopeConfig->getValue(PATH_TITLE, ScopeInterface::SCOPE_STORE));die;
         
         return $config;
