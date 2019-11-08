694948c0d04458701bc746ad66f2260d8ba3542e
edusorcerer <eduardofidera@gmail.com>
2019-11-08 11:06:38 -0300

    Add function to retrieve token based on the customer state

--- Model/Core.php
+++ Model/Core.php
@@ -4,47 +4,59 @@ namespace Avanti\RewriteMercadoPago\Model;
 
 use Magento\Store\Model\ScopeInterface;
 
-const PATH_TITLE = 'rewritemercadopago/general/auth_token';
+const ADMIN_CREDENTIALS_PATH = 'rewritemercadopago/general/';
 
-const ALL_STATES = array(
-    "AC" => "Acre",
-    "AL" => "Alagoas",
-    "AM" => "Amazonas",
-    "AP" => "Amapá",
-    "BA" => "Bahia",
-    "CE" => "Ceará",
-    "DF" => "Distrito Federal",
-    "ES" => "Espírito Santo",
-    "GO" => "Goiás",
-    "MA" => "Maranhão",
-    "MG" => "Minas Gerais",
-    "MS" => "Mato Grosso do Sul",
-    "MT" => "Mato Grosso",
-    "PA" => "Pará",
-    "PB" => "Paraíba",
-    "PE" => "Pernambuco",
-    "PI" => "Piauí",
-    "PR" => "Paraná",
-    "RJ" => "Rio de Janeiro",
-    "RN" => "Rio Grande do Norte",
-    "RO" => "Rondônia",
-    "RR" => "Roraima",
-    "RS" => "Rio Grande do Sul",
-    "SC" => "Santa Catarina",
-    "SE" => "Sergipe",
-    "SP" => "São Paulo",
-    "TO" => "Tocantins",
-);
+const STATES_BY_REGIONS = array(
+    ["AC", "AL", "AM", "AP", "BA", "CE", "DF", "ES", "GO"],
+    ["MA", "MG", "MS", "MT", "PA", "PB", "PE", "PI", "PR"],
+    ["RJ", "RN", "RO", "RR", "RS", "SC", "SE", "SP", "TO"]
+)
 
-const MAPPED_REGIONS = array(
-    0 => array(
-        'name' => 'Sul',
-        'states' => ['Santa Catarina', 'Rio Grande do Sul']
-    )
+const MAPPED_CREDENTIALS_PATH_BY_STATE = array(
+    'auth_token_region1' => STATES_BY_REGIONS[0],
+    'auth_token_region2' => STATES_BY_REGIONS[1],
+    'auth_token_region3' => STATES_BY_REGIONS[2]
 );
 
 class Core extends \MercadoPago\Core\Model\Core
 {
+    /**
+     * Get the access token from configs based on the customer address state
+     * 
+     * @param $preference
+     * 
+     * @return string||boolean
+     */
+    public function getAccessToken($preference)
+    {
+        /**
+         * Does nothing if there is no address key on the preference
+         */
+        if (!array_key_exists('payer', $preference) || !array_key_exists('address', $preference['payer'])) return false;
+
+        $address = $preference['payer']['address'];
+        $state = $address['federal_unit'] ?: false
+
+        if (!$state) return false
+
+        $this->_coreHelper->log("Preference address federal_unit - ", 'mercadopago-custom.log', $state);
+
+        /**
+         * Finds the path to an access token based on a address state
+         */
+        $accessTokenPath = null;
+        foreach(MAPPED_CREDENTIALS_PATH_BY_STATE as $path => $states) {
+            if (array_search('SC', $states) !== false) {
+                $accessTokenPath = $path;
+                break;
+            }
+        }
+
+        $accessToken = $this->_scopeConfig->getValue(ADMIN_CREDENTIALS_PATH . $accessTokenPath, ScopeInterface::SCOPE_STORE);
+
+        die('wtf');
+        return $accessToken;
+    }
 
     /**
      * Return response of api to a preference
@@ -57,7 +69,9 @@ class Core extends \MercadoPago\Core\Model\Core
      */
     public function postPaymentV1($preference)
     {
-        $this->_accessToken = 'TESTE';
+        $this->_accessToken = $this->getAccessToken($preference);
+
+        $this->_coreHelper->log("Access Token for Post from rewrite - ", 'mercadopago-custom.log', $this->_accessToken);
 
         $response = parent::postPaymentV1($preference);
 
